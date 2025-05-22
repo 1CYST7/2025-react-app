32baa506c1bef25939e44b26131e4c663cf2253e
1cyst7 <5b1g0020@office.stust.edu.tw>
2025-05-22 09:47:04 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import MySlider from "./component/MySlider";
+import MyPanel from "./component/MyPanel";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,13 +18,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>5b1g0020胡芊杏</h1>
+      <h1>123456789陳小明</h1>
       <h2>我的第一個元件：Slider</h2>
       <div>
         <p>R:<MySlider /></p>
         <p>G:<MySlider /></p>
         <p>B:<MySlider /></p>
       </div>
+      <br/>
+      <h2>第二個元件：RGB色彩面板</h2>
+      <MyPanel />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
